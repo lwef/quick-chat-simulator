7138a949c29e97be7249910139ac5a0cd6deeaff
lwef <linapple1234@icloud.com>
2022-06-27 19:04:00 +0900

    #2 Add color picker2

--- src/view/Main.js
+++ src/view/Main.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Footer from '../component/Footer';
 import './Main.css';
+import { ChromePicker } from 'react-color';
 
 function Main() {
+    const [color, setColor] = useState('');
+    
     const colorPicker1 = () => {
-        const color = ["red", "yellow", "blue", "aqua", "olive", "silver", "maroon", "navy", "white", "black",
+        const colorSet = ["red", "yellow", "blue", "aqua", "olive", "silver", "maroon", "navy", "white", "black",
             "brown", "darkblue", "magenta", "green", "grey", "lightblue", "lime", "orange", "purple", "teal"];
         const result = [];
-        const numOfColor = color.length;
+        const numOfColor = colorSet.length;
         for (let i=0; i<numOfColor; i++) {
-            result.push(<div key={i} style={{background:color[i]}} onClick = {()=> {console.log(color[i])}}></div>);
+            result.push(<div key={i} style={{background:colorSet[i]}} onClick = {()=> {
+                console.log(colorSet[i]);
+                setColor(colorSet[i]);
+            }}></div>);
         }
         return result;
     }
+
+    useEffect(() => {
+        if (!color) {
+          setColor('');
+        }
+        setColor(color);
+    }, [color]);
+
+    const handleColorChange = useCallback((color) => {
+        setColor(color);
+    }, [color]) //컬러 데이터가 바뀔때마다 이 함수는 갱신된다.
 
     const chatSize = () => {
         const result = [];
@@ -25,14 +42,24 @@ function Main() {
     return (
         <div className='main'>
             <Footer/>
-            <div className='main__colorPick'>
+            <div className='main__colorPick' style={{background:color}}>
                 choose color #1<br/>
                 <div className='colorWrap'>
                     {colorPicker1()}
                 </div>
             </div>
-            <div className='main__colorPick'>
-                choose color #2
+            <div className='main__colorPick2' style={{background:color}}>
+                <center>
+                choose color #2<br/>
+                <ChromePicker
+                    color={color}
+                    onChange={color => handleColorChange(color.hex)}
+                />
+                <input
+                    value={color}
+                    onChange={e => handleColorChange(e.target.value)}
+                />
+                </center>
             </div>
             <div className='main__smallBox'>
                 select bold ?
