8f94c1e71c9da4fb15882dfab50ec5b4fe7dae8b
lwef <linapple1234@icloud.com>
2022-06-27 15:10:33 +0900

    #1 make footer

---
+++ src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer__show">
+                this is footer
+            </div>
+            <div className="footer__code">
+                code
+            </div>
+            <div className="footer__copy">
+                copy
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/view/Main.js
+++ src/view/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../component/Footer';
 
 function Main() {
     return (
         <div>
             this is mainpage
+            <Footer/>
         </div>
     )
 }
