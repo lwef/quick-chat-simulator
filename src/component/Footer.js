3d8a3d57c34ed96cdf2c7ce2cf1dc8b221472e95
lwef <linapple1234@icloud.com>
2022-06-30 00:30:50 +0900

    #5 Generate code

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,31 +1,64 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Footer.css'
 
 const Footer = (data) => {
     const [code, setCode] = useState('');
-    console.log(data); //전송된 데이터 확인
-    const onChangeCode = (e) => { setCode(e.target.value) };
+    const [word, setWord] = useState('');
+    //console.log(data.sendData); //전송된 데이터
+    const userData = data.sendData;
+
+    useEffect(() => {
+        var myCode = word;
+        if(userData[0] != false) {
+            myCode = "<color=" + userData[0] + ">" + myCode + "</color>";
+        }
+        if(userData[1] === true) {
+            myCode = "<b>" + myCode + "</b>";
+        }
+        if(userData[2] !== 0) {
+            myCode = "<size=" + userData[2] + ">" + myCode + "</size>";
+        }
+        if(userData[3] === true) {
+            myCode = "<i>" + myCode + "</i>";
+        }
+        //console.log(myCode);
+        setCode(myCode);
+
+    }, [data, word]);
+
+    useEffect(() => {
+        if (!code) {
+          setCode('');
+        }
+        setCode(code);
+    }, [code]);
+
+
+    const onChangeWord = (e) => { 
+        setWord(e.target.value);
+    };
+    /*
     const handleCodeChange = (param) => {
         setCode(param);
     }
+    */
 
     return (
         <div className="footer">
             <div className="footer__show">
                 this is footer
             </div>
-            <input placeholder='write your word' value={code} onChange={onChangeCode}/>
+            <input placeholder='write your word' value={word} onChange={onChangeWord}/>
             <input 
                 className="footer__code" 
                 placeholder='code'
                 value={code} /* value={data.sendData} */
-                onChange={e => handleCodeChange(e.target.value)}
+                readOnly
             />
-            <button>
+            <button className="footer__copy">
                 copy
             </button>
         </div>
     )
 }
-//<button className="footer__copy">
 export default Footer;
