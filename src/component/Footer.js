37ac9a4ff42f129487fecc69daec8ef4ed9d6546
lwef <linapple1234@icloud.com>
2022-07-01 14:01:02 +0900

    #6 show custom chat result

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,25 +4,41 @@ import './Footer.css'
 const Footer = (data) => {
     const [code, setCode] = useState('');
     const [word, setWord] = useState('');
+    const [showStyle, setShowStyle] = useState(
+        {
+            color: "#F2F5A9",
+            fontWeight: null,
+            fontStyle: null,
+            fontSize: "18px"
+        }
+    );
     //console.log(data.sendData); //전송된 데이터
     const userData = data.sendData;
 
     useEffect(() => {
         var myCode = word;
-        if(userData[0] != false) {
+        var showResult = [];
+
+        if(userData[0] !== false) {
             myCode = "<color=" + userData[0] + ">" + myCode + "</color>";
+            showResult.color = userData[0];
         }
         if(userData[1] === true) {
             myCode = "<b>" + myCode + "</b>";
+            showResult.fontWeight = "bold";
         }
         if(userData[2] !== 0) {
             myCode = "<size=" + userData[2] + ">" + myCode + "</size>";
+            showResult.fontSize = userData[2] + "px";
         }
         if(userData[3] === true) {
             myCode = "<i>" + myCode + "</i>";
+            showResult.fontStyle = "italic";
         }
         //console.log(myCode);
+        //console.log(showResult);
         setCode(myCode);
+        setShowStyle(showResult);
 
     }, [data, word]);
 
@@ -70,8 +86,8 @@ const Footer = (data) => {
 
     return (
         <div className="footer">
-            <div className="footer__show">
-                this is footer
+            <div style={showStyle} className="footer__show" >
+                {word}
             </div>
             <input placeholder='write your word' value={word} maxLength = '30' onChange={onChangeWord}/>
             <div>
