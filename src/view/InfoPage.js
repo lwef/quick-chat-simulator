2c2b89c738c09199759b114e19b4c79e75aca1b4
lwef <linapple1234@icloud.com>
2022-07-03 19:14:39 +0900

    #8 Make infopage

--- src/view/InfoPage.js
+++ src/view/InfoPage.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './InfoPage.css';
+import License from './License';
 
 function InfoPage() {
+
+    const licenseData = [
+        {
+            type: "Apache License 2.0",
+            title: "web-vitals",
+            homepage: "https://github.com/GoogleChrome/web-vitals",
+            copyright: "Copyright 2020 Google LLC"
+        },
+        {
+            type: "mit",
+            title: "@testing-library/user-event",
+            homepage: "https://github.com/testing-library/user-event",
+            copyright: "Copyright (c) 2020 Giorgio Polvara"
+        }
+    ]
+
     return (
-        <div>
-            this is info page
+    <div>
+        <div id='intro'>
+            This is fan website of the game "Castle Defense Online".
         </div>
+        <div>
+            <h2>Open Source License</h2>
+            <License data={licenseData[0]}/>
+            <License data={licenseData[1]}/>
+        </div>
+        <div>
+            <h2>License</h2>
+        </div>
+        <div>
+            <h2>Developer</h2>
+        </div>
+    </div>
     )
 }
 
